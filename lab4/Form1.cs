fce614940a1fa0ada3839f926d8d50aeaf51399d
kekwem <93526125+kekwem@users.noreply.github.com>
2021-11-01 19:40:19 +0100

    Add files via upload

--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -16,5 +16,37 @@ namespace lab4
         {
             InitializeComponent();
         }
+
+        Drink drink = null;
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            if (sizeOfVeselBox.Text.Equals("") || percentOfAlchBox.Text.Equals("") || amountOfVeselsBox.Equals(""))
+            {
+                valueOfDrinksBox.Text = "Mistake";
+                valueOfPureAlchBox.Text = "Mistake";
+            }
+            else
+            {
+                double _size = double.Parse(sizeOfVeselBox.Text);
+                double _percent = double.Parse(percentOfAlchBox.Text);
+                double _number = double.Parse(amountOfVeselsBox.Text);
+                drink = new Drink(_size, _percent, _number);
+
+                valueOfDrinksBox.Text = drink.DrinkSize().ToString();
+                valueOfPureAlchBox.Text = drink.DrinkAlcoholSize().ToString();
+            }
+            
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            drink = null;
+            sizeOfVeselBox.Text = "";
+            percentOfAlchBox.Text = "";
+            amountOfVeselsBox.Text = "";
+            valueOfDrinksBox.Text = "";
+            valueOfPureAlchBox.Text = "";
+        }
     }
 }
