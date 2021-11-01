b157f83d1b4b7e3051ff7744127b1cf9a9613094
Drun4 <isaenkoandrej111@gmail.com>
2021-11-01 20:54:51 +0300

    add some methods

---
+++ lab4/Drink.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Drink
+    {
+        private double size;
+        private double percent;
+        private double number;
+
+        public Drink(double _size, double _percent, double _number)
+        {
+            size = _size;
+            percent = _percent;
+            number = _number;
+        }
+
+        public double DrinkSize()
+        {
+            return size * number;
+        }
+
+        public double DrinkAlcoholSize()
+        {
+            return (size * number) / (percent / 100);
+        }
+    }
+}
